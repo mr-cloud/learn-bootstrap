7b7393bdd16decb09e782cd31e4bc1189bf94c3f
leo <leozhangsjtu@qq.com>
2018-12-26 08:36:50 +0800

    learning npm-scripts

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,19 @@ $(document).ready(function() {
 	$('#reserveBtn').click(function(){
 		$('#reserveModal').modal();
 	});
+	$('#mycarousel').carousel({interval: 2000});
+	$('#carouselButton').click(function () {
+		if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+			$('#mycarousel').carousel('pause');
+			$('#carouselButton').children('span').removeClass('fa-pause');
+			$('#carouselButton').children('span').addClass('fa-play');
+			console.log("paused carousel.")
+		}
+		else  {
+			$('#mycarousel').carousel('cycle');
+			$('#carouselButton').children('span').removeClass('fa-play');
+			$('#carouselButton').children('span').addClass('fa-pause');
+			console.log("cycling carousel.")
+		}
+	});
 });
